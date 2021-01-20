6c8356120889951004345842073f3abd864e7c2e
Michele Caputo <michele@caputoweb.xyz>
2021-01-20 21:25:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,9 +14,4 @@ sudo apt upgrade -y
 echo "Installing components"
 sudo ./install-bluetooth.sh
 sudo ./install-shairport.sh
-sudo ./install-spotify.sh
-sudo ./install-upnp.sh
-sudo ./install-snapcast-client.sh
-sudo ./install-pivumeter.sh
-sudo ./enable-hifiberry.sh
 sudo ./enable-read-only.sh
